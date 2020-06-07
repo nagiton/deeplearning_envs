be346fd44f479fd02d4fe137b4e3e9dca9e5aa68
nabion <d.watanabe829@gmail.com>
2020-06-08 00:35:42 +0900

    installed python in base image

--- cuda10.dockerfile
+++ cuda10.dockerfile
@@ -2,3 +2,11 @@ FROM nvidia/cuda:10.2-cudnn7-devel
 
 RUN apt update
 RUN apt install sudo
+
+COPY install_essentials.sh /tmp/install_essensitals.sh
+RUN yes "Y" | /tmp/install_essensitals.sh
+
+COPY install_libraries.sh /tmp/install_libraries.sh
+RUN yes "Y" | /tmp/install_libraries.sh
+
+CMD ["bash"]
---
+++ install_essentials.sh
@@ -0,0 +1,20 @@
+sudo apt install --no-install-recommends \
+    git \
+    python-pip \
+    python-dev \
+    python3-distutils \
+    build-essential \
+
+sudo apt-get install software-properties-common python-software-properties
+sudo add-apt-repository ppa:jonathonf/python-3.6
+sudo apt-get update
+sudo apt-get install python3.6 python3.6-dev
+sudo apt-get install -y wget
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3.6 get-pip.py
+
+
+sudo pip3 install jupyterlab
+
+
+
